565703fb6f09e4412fb4e7c5ba260f3d70348274
Kay0s <hamilton.kristina@gmail.com>
2020-12-23 16:40:59 -0600

    added db and connection.js

---
+++ db/connection.js
@@ -0,0 +1,20 @@
+// MySQL DB Connection Information
+let mysql = require("mysql");
+
+let connection = mysql.createConnection({
+  port: 3306,
+  user: "root",
+  password: "puppies",
+  database: "employeetracker_db",
+});
+
+// Initiate MySQL Connection.
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
