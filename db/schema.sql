277ba540d94fdfb3d0140f45be7bc5ab1919f6c3
Kay0s <hamilton.kristina@gmail.com>
2020-12-27 18:23:08 -0600

    added select from on schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,7 @@ USE employeetracker_db;
 
 
 CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name varchar(30) NOT NULL,
   last_name varchar(30) NOT NULL,
   role_id INT,
@@ -33,36 +33,6 @@ CREATE TABLE role (
 );
 
 
-
-CREATE TABLE employe_info(
-  employee_id int,
-   first_name varchar(30) NOT NULL,
-  last_name varchar(30) NOT NULL,
-  titel varchar(30) NOT NULL,
-  department_name varchar(30) NOT NULL,
-  salary decimal NOT NULL,
-  manager varchar(30,
-  foreign key (employe_id) REFERENCES employee (id) ON DELETE CASCADE});
-
-  SELECT employee.id, employee.first_name, employee.last_name, role.title, department.department_name,
-  role.salary, employee.manager_id
-  FROM employee
-  INNER JOIN employee_info ON employee_info.employee_id = employee.depart_id
-  INNER JOIN employee_info ON employee_info.first_name = employee.first_name
-  INNER JOIN employee_info ON employee_info.last_name = employee.last_name;
-    INNER JOIN employee_info ON employee_info.manager = employee.manager_id;
-
-    SELECT role.title, role.salary
-
-  FROM role
-  INNER JOIN employee_info ei_alias1 ON employee_info.title=role.title
-  INNER JOIN employee_info ON employee_info.salary= role.salary;
-
-
-    SELECT department.name
-  FROM department
-  INNER JOIN employee_info ON employee_info.department_name = role.department;
-
-
-
-  SELECT employee.id, first_name, last_name, title, salary, manager
+SELECT * FROM employee;
+SELECT * FROM department;
+SELECT * FROM role;
